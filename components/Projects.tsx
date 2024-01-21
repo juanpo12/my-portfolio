3320b25edfc84c11adb3960c227004439b9474e3
juanpo12 <juanjoc2000@hotmail.com>
2024-01-21 18:09:36 -0300

    feat[Projects]:Added two buttons for bottom page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Image, Link } from "@nextui-org/react"
+import { Button, Image, Link } from "@nextui-org/react"
 import NextLink from "next/link"
 import NextImage from "next/image"
 import { useState } from "react"
@@ -16,7 +16,7 @@ const Projects = () => {
     const [projects, setProject] = useState<Project[]>([
         {
             title: "Alma Canabica",
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique asperiores temporibus expedita nostrum eius commodi, deleniti cum voluptate minus sit ratione eum beatae tenetur, cumque adipisci rem ipsam natus saepe.',
+            description: 'description 1',
             image: "/alma.png",
             deployUrl: "project1.com",
             repoUrl: "https://github.com/QuickCodeFusion/Alma-Cannabica"
@@ -51,17 +51,27 @@ const Projects = () => {
             <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-4">
                 {
                     projects.map((project) => (
-                        <div key={project.title} className="flex flex-col md:mb-4 justify-center items-center md:w-1/3 bg-gray-800 bg-opacity-50 rounded-3xl p-5">
-                            <Link className="w-fit flex flex-col" as={NextLink} href={project.deployUrl}>
+                        <div key={project.title} className="flex flex-col md:mb-4 items-center md:w-1/3 bg-gray-800 bg-opacity-50 rounded-3xl pt-5">
+                            <Link className="w-fit flex flex-col p-2 text-white" as={NextLink} href={project.deployUrl}>
                                 <Image as={NextImage} src={project.image} alt={project.title} width={400} height={400} />
                                 <div className="p-4">
                                     <h2 className="text-xl font-Raleway">{project.title}</h2>
                                     <p className="text-lg font-mono">{project.description}</p>
                                 </div>
                             </Link>
-                            <Link className="w-fit" as={NextLink} href={project.repoUrl}>
+                            {/* <Link className="w-fit" as={NextLink} href={project.repoUrl}>
                                 <Image as={NextImage} src='/github(3).png' alt={project.title} width={50} height={50}></Image>
-                            </Link>    
+                            </Link>     */}
+                            <div className="flex border-t-small border-gray-700 w-full h-12 ">
+                                <Button className="flex w-[50%] justify-center border-r border-gray-700 items-center gap-3 rounded-none bg-inherit"> 
+                                    <Image as={NextImage} src='/github(3).png' alt={project.title} width={30} height={50}></Image>
+                                        <p className="text-blue-400">Repository</p>
+                                </Button>
+                                <Button className="flex w-[50%] justify-center items-center gap-3 rounded-none bg-inherit"> 
+                                    <Image as={NextImage} src='/github(3).png' alt={project.title} width={30} height={50}></Image>
+                                        <p className="text-blue-400">Repository</p>
+                                </Button>
+                            </div>
                         </div>
                     ))
                 }
