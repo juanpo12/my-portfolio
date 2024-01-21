9c21abb0ba6cb36f3d124bd359220d40ea64c833
juanpo12 <juanjoc2000@hotmail.com>
2024-01-21 18:08:50 -0300

    fix[Profile]: fixed sizes

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -5,8 +5,8 @@ import NextImage from "next/image"
 const Profile = () => {
 
     return (
-            <div className="w-full flex justify-center items-center h-screen">
-                <div className="flex flex-col justify-center items-center p-3 rounded-lg h-[70%] w-2/3">
+            <div className="w-full flex justify-center items-center h-full">
+                <div className="flex flex-col justify-center items-center p-3 rounded-lg w-2/3">
                         <span className="flex justify-center">
                             <h3 className="relative mt-10 md:mt-0 py-1 w-[max-content] text-center font-mono text-4xl
                                 before:absolute before:inset-0 before:animate-typewriter
@@ -14,19 +14,19 @@ const Profile = () => {
                                 after:absolute after:inset-0 after:w-[0.125em] after:animate-caret
                                 after:bg-white">Hello there 👋</h3>
                         </span>
-                    <div className="flex flex-col justify-center items-center md:h-[60vh] gap-10">
+                    <div className="flex flex-col justify-center items-center  gap-10">
                             <Image as={NextImage} alt="profile" src="/sinFondo.jpg" className="rounded-full shadow-2xl size-4/4 " width={300} height={300}></Image>
                         <div className="flex flex-col justify-center text-center">
                             <h1 className="text-2xl font-mono">Juan José Diaz</h1>
-                            <h2 className=" font-mono">FullStack Developer</h2>
-                            <Spacer y={3}></Spacer>
+                            <h2 className=" font-mono text-blue-400">FullStack Developer</h2>
+                            <Spacer y={6}></Spacer>
                             <h4 className="text-xl font-mono">I create exceptional web experiences, design attractive interfaces and <br />
                                 develop web 
                                 applications with passion.
                                 Specialized in optimizing the user experience.
                             </h4>
                             <Spacer y={3}></Spacer>
-                            <p>
+                            <p className="text-blue-400"> 
                                 I use technologies like Firebase, TypeScript, Next.js, and Redux Toolkit.
                                 Your next web project begins here.
                             </p>
