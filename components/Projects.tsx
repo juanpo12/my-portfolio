24f3391a8dd9f16070c2ecf11490971865489f20
juanpo12 <juanjoc2000@hotmail.com>
2024-03-05 14:50:14 -0300

    feat: Ajusted size from project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Image, Link } from "@nextui-org/react"
+import { Accordion, AccordionItem, Button, Image, Link } from "@nextui-org/react"
 import NextLink from "next/link"
 import NextImage from "next/image"
 import { useState } from "react"
@@ -18,32 +18,31 @@ const Projects = () => {
             title: "Alma Canabica",
             description: 'description 1',
             image: "/alma.png",
-            deployUrl: "project1.com",
+            deployUrl: "https://alma-cannabica-1b86qh0y9-juans-projects-ff191d93.vercel.app/",
             repoUrl: "https://github.com/QuickCodeFusion/Alma-Cannabica"
         },
         {
             title: "Discount Dash",
-            description: "Description 1",
+            description: "Discount Dash is an application that sells products nearing their expiration dates at reduced prices, helping to minimize food waste and save money. It offers a diverse range of products and encourages local shopping, providing both economic and ecological benefits.",
             image: "/discountDash.png",
             deployUrl: "https://discountdash.vercel.app/",
             repoUrl: "https://github.com/UFCampos/Discount-Dash"
         },
         {
-            title: "PI Countries",
+            title: "Cripto Currencies",
             description: "Description 1",
-            image: "/alma.png",
+            image: "/sumak.png",
             deployUrl: "project1.com",
-            repoUrl: "project1.com"
+            repoUrl: "https://github.com/QuickCodeFusion/sumak-kawsay-web"
         },
         {
-            title: "Alma CanabicaFIN",
-            description: "Description 1",
+            title: "Not available yet",
+            description: "NONE",
             image: "/alma.png",
             deployUrl: "project1.com",
             repoUrl: "SOYELREPO.com"
         }
     ])
-
 
     return(
         <div id="projects" className="md:grid flex flex-col md:grid-row-3 justify-center text-center gap-6 mt-6  w-full p-5" >
@@ -52,25 +51,28 @@ const Projects = () => {
                 {
                     projects.map((project) => (
                         <div key={project.title} className="flex flex-col md:mb-4 items-center md:w-1/3 bg-gray-800 bg-opacity-50 rounded-3xl pt-5">
-                            <Link className="w-fit flex flex-col p-2 text-white" as={NextLink} href={project.deployUrl}>
-                                <Image as={NextImage} src={project.image} alt={project.title} width={400} height={400} />
+                            <div className="w-fit flex flex-col p-2 text-white">
+                                <div className="">
+                                    <Image as={NextImage} className="hover:scale-110 cursor-pointer" src={project.image} alt={project.title} width={400} height={400} />
+                                </div>
                                 <div className="p-4">
                                     <h2 className="text-xl font-Raleway">{project.title}</h2>
-                                    <p className="text-lg font-mono">{project.description}</p>
+                                            <p className="text-lg font-mono">{project.description}</p>
                                 </div>
-                            </Link>
-                            {/* <Link className="w-fit" as={NextLink} href={project.repoUrl}>
-                                <Image as={NextImage} src='/github(3).png' alt={project.title} width={50} height={50}></Image>
-                            </Link>     */}
-                            <div className="flex border-t-small border-gray-700 w-full h-12 ">
-                                <Button className="flex w-[50%] justify-center border-r border-gray-700 items-center gap-3 rounded-none bg-inherit"> 
-                                    <Image as={NextImage} src='/github(3).png' alt={project.title} width={30} height={50}></Image>
-                                        <p className="text-blue-400">Repository</p>
-                                </Button>
-                                <Button className="flex w-[50%] justify-center items-center gap-3 rounded-none bg-inherit"> 
-                                    <Image as={NextImage} src='/github(3).png' alt={project.title} width={30} height={50}></Image>
-                                        <p className="text-blue-400">Repository</p>
-                                </Button>
+                            </div>
+                            <div className="flex m-2 h-full border-gray-700 items-end  w-full">
+                                    <Button className="flex w-1/2 justify-center border-t-small border-r border-gray-700 items-center gap-3 rounded-none bg-inherit"> 
+                                        <Link target="_blank" className="flex gap-4" as={NextLink} href={project.repoUrl}>
+                                            <Image as={NextImage} src='/github(3).png' alt={project.title} width={30} height={50}></Image>
+                                                <p className="text-blue-400">Repository</p>
+                                        </Link>
+                                    </Button>
+                                    <Button className="flex w-1/2 border-t-small border-gray-700 justify-center items-center gap-3 rounded-none bg-inherit"> 
+                                        <Link target="_blank" className="flex gap-4" as={NextLink} href={project.deployUrl}>
+                                            <Image as={NextImage} src='/web3.png' alt={project.title} width={30} height={50}></Image>
+                                            <p className="text-blue-400">Deploy</p>
+                                        </Link>
+                                    </Button>
                             </div>
                         </div>
                     ))
