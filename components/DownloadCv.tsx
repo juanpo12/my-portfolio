2e972b4af4d38520173a2ac0287f2512b8e30ff6
juanpo12 <juanjoc2000@hotmail.com>
2024-09-24 21:21:28 -0300

    updated resume

--- components/DownloadCv.tsx
+++ components/DownloadCv.tsx
@@ -1,5 +1,4 @@
-import { Button, Image, Link } from "@nextui-org/react"
-import NextImage from "next/image"
+import { Image, Link } from "@nextui-org/react"
 
 const DownloadCv = () => {
 
@@ -7,7 +6,7 @@ const DownloadCv = () => {
     return (
         <div className="flex justify-center p-4" >
             <div className="flex flex-col bg-gray-800 bg-opacity-50 rounded-3xl p-4 gap-6">
-                <Link target="_blank" href="/cv.pdf" >
+                <Link target="_blank" href="/Resume.pdf" >
                     <Image className=" hover:scale-110 cursor-pointer" src="/panda.png" alt="download" width={400} height={800}></Image>
                 </Link>
             </div>
